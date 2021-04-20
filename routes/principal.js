7965b0f3b61f8103748b9d3795b96c74f51b91e5
jafet5757 <moranorozcokevinjafet@gmail.com>
2021-04-20 00:09:05 -0500

    Cambios a la interfaz de edit empleado

--- routes/principal.js
+++ routes/principal.js
@@ -8,7 +8,7 @@ const departaments = require('../controller/departaments.js');
 router.get('/',(req,res) => {
     res.render('index', {});
 });
-
+//INSERT INTO employees VALUES(0,'2021-04-20', 'Juan', 'Perez', 'M', '2021-04-20');
 router.post('/getDepartaments', departaments.getDepartaments);
 router.post('/addNewDepartament',departaments.addNewDepartament);
 router.post('/deleteDepartament', departaments.deleteDepartament);
